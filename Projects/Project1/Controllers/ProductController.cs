416b391477484556b39c285c4b7b6c25c81e6255
Fahad Hasan <hasan.fahad67@outlook.com>
2026-02-14 22:05:12 +0600

    Create Operation

---
+++ Projects/Project1/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project1.Data;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController (AppDbContext context)
+        {
+            _context = context;
+        }
+    
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid) return View(product);
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
